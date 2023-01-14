78db9dc6ae5cfcfc5d5f60891092a44128c8569c
mengiefen <mengiefen@gmail.com>
2023-01-14 22:09:06 +0300

    modify server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-const favoriteColor = process.env.FAVORITE_COLOR;
+const FAVORITE_COLOR = process.env.FAVORITE_COLOR;
 
 async function main() {
   while (true) {
     console.log('Microservices rock!');
-    console.log(`My favorite color is ${favoriteColor}`);
+    console.log(`My favorite color is ${FAVORITE_COLOR}`);
     await sleep(5000);
   }
 }
